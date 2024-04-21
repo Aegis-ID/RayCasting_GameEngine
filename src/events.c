63f07eb542856a25f85d245851737c42676b02a4
Aegis-ID <pierre.nguyen@epitech.eu>
2024-04-21 20:47:59 +0200

    {feat} add strafing

--- src/events.c
+++ src/events.c
@@ -8,8 +8,9 @@
 #include <SFML/Graphics.h>
 #include <SFML/System.h>
 #include <SFML/OpenGL.h>
-#include "global.h"
 #include <math.h>
+#include "global.h"
+#include "settings.h"
 
 static void p_movement(sfEvent *event, player_t *p)
 {
@@ -17,20 +18,28 @@ static void p_movement(sfEvent *event, player_t *p)
         p->pos.x += p->delta.x;
         p->pos.y += p->delta.y;
     }
+    if (event->key.code == sfKeyQ) {
+        p->pos.x += p->delta.y;
+        p->pos.y -= p->delta.x;
+    }
     if (event->key.code == sfKeyS) {
         p->pos.x -= p->delta.x;
         p->pos.y -= p->delta.y;
+    }
+    if (event->key.code == sfKeyD) {
+        p->pos.x -= p->delta.y;
+        p->pos.y += p->delta.x;
     }
 }
 
 static void p_rotation(sfEvent *event, player_t *p)
 {
-    if (event->key.code == sfKeyQ) {
+    if (event->key.code == sfKeyA) {
         p->angle -= 0.1;
         if (p->angle < 0)
             p->angle += 2 * M_PI;
     }
-    if (event->key.code == sfKeyD) {
+    if (event->key.code == sfKeyE) {
         p->angle += 0.1;
         if (p->angle > (2 * M_PI))
             p->angle -= 2 * M_PI;
@@ -46,7 +55,7 @@ void events(game_t *game, player_t *player)
 
     if (event.type == sfEvtClosed || event.key.code == sfKeyEscape)
         sfRenderWindow_close(window);
-    else if (event.type == sfEvtResized)
+    if (event.type == sfEvtResized)
         glViewport(0, 0, event.size.width, event.size.height);
     p_movement(&event, player);
     p_rotation(&event, player);
