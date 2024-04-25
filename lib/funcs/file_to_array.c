9a19cc90832b9c27f0d4af890da0111195571673
Aegis-ID <pierre.nguyen@epitech.eu>
2024-04-25 08:43:52 +0200

    {fix} malloc after cond

--- lib/funcs/file_to_array.c
+++ lib/funcs/file_to_array.c
@@ -10,7 +10,6 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "lib.h"
 
 static off_t get_size(const char *filepath)
 {
@@ -28,7 +27,7 @@ char **file_to_array(const char *filepath, const char *delim)
     int fd = 0;
     size_t size = 0;
     off_t file_size = get_size(filepath);
-    char *buffer = (char *) malloc(sizeof(char) * (file_size + 1));
+    char *buffer = NULL;
     char **array = NULL;
 
     fd = open(filepath, O_RDONLY);
@@ -36,6 +35,7 @@ char **file_to_array(const char *filepath, const char *delim)
         dprintf(2, "File opening failed\n");
         return NULL;
     }
+    buffer = (char *) malloc(sizeof(char) * (file_size + 1))
     size = read(fd, buffer, (file_size - 1));
     buffer[size] = '\0';
     array = str_to_word_array(buffer, delim);
